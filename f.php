394b4737f277c68263483b9c64a395b53f720349
69169 <69169a@gmail.com>
2018-11-01 14:03:47 +0500

    Making cards by a

--- f.php
+++ f.php
@@ -81,7 +81,7 @@ function selectData(){
 	$conn->close();
 }
 
-selectData();
+//selectData();
 
 function selectDataHelper($mCategories){
   
@@ -99,26 +99,49 @@ function selectDataHelper($mCategories){
 
 	if ($result->num_rows > 0) {
       
-      echo "total num of rows $result->num_rows";
+      //echo "total num of rows $result->num_rows";
       
       for($i = 0; $i < $result->num_rows/2; $i++){
-				echo "<br>for counter is : $i<br>";
-        $row = $result->fetch_assoc();
-        echo $row["$table1Col2"];
+		  
+		  	$row = $result->fetch_assoc();
+			$s_Name_1 = $row["$table1Col2"];
+			$s_link_1 = $row["$table1Col3"];
+			$s_img_link_1 = $row["$table1Col4"];
+		  
+			echo ('
+			<div class="card-deck">
+				  
+				<div class="card">
+					<img class="card-img-top" src="'.$s_img_link_1.'" alt="Card image" style="width:100%">
+					<div class="card-body">
+						  <h4 class="card-title">John Doe</h4>
+						  <p class="card-text">Some example text some example text. John Doe is an architect and engineer</p>
+						  <a href="#" class="btn btn-primary">See Profile</a>
+					</div>
+  				</div>
+				
+				<div class="card">
+					<img class="card-img-top" src="img_avatar1.png" alt="Card image" style="width:100%">
+					<div class="card-body">
+						  <h4 class="card-title">John Doe</h4>
+						  <p class="card-text">Some example text some example text. John Doe is an architect and engineer</p>
+						  <a href="#" class="btn btn-primary">See Profile</a>
+					</div>
+  				</div>
+			
+			</div>
+			');
+		  
+		
+		  	$row = $result->fetch_assoc();
+			echo $row["$table1Col2"];
 			echo("<br>");
 			echo $row["$table1Col3"];
 			echo("<br>");
 			echo $row["$table1Col4"];
 			echo("<br>");
-        $row = $result->fetch_assoc();
-        echo $row["$table1Col2"];
-			echo("<br>");
-			echo $row["$table1Col3"];
-			echo("<br>");
-			echo $row["$table1Col4"];
-			echo("<br>");
-			}
-      
+		}
+
 		// output data of each row
 		//while($row = $result->fetch_assoc()) {
           
